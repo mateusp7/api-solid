467d461453bffe0345fe6f217fd8db8d19a93438
mateusp7 <mateusdev1337@gmail.com>
2025-12-15 11:41:52 -0300

    chore(lint): fix some files

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,8 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+import z from "zod";
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
 import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
 import { RegisterUseCase } from "@/use-cases/register";
-import type { FastifyReply, FastifyRequest } from "fastify";
-import z from "zod";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
 	const registerBodySchema = z.object({
